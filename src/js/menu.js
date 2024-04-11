61298581a811b136eb69fd1067da37df5691cc56
fedmike <molnek87@mail.ru>
2024-04-11 09:54:23 +0900

    zxc

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,15 +1,12 @@
-export const menus = () => {
-    const headerMenuDesktopItems = document.querySelectorAll('.header-menu__desktop-item--link');
+const headerMenuDesktopItems = document.querySelectorAll('.header-menu__desktop-item--link');
 
-    console.log(headerMenuDesktopItems);
-    headerMenuDesktopItems.forEach((item) => {
-        item.addEventListener('mouseenter', (e) => {
-            headerMenuDesktopItems.forEach((item2) => {
-                if (item2.classList.contains('active')) {
-                    item2.classList.remove('active');
-                }
-            });
-            item.classList.add('active');
-        })
+headerMenuDesktopItems.forEach((item) => {
+    item.addEventListener('mouseenter', (e) => {
+        headerMenuDesktopItems.forEach((item2) => {
+            if (item2.classList.contains('active')) {
+                item2.classList.remove('active');
+            }
+        });
+        item.classList.add('active');
     })
-}
+})
