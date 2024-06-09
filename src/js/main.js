894c13b69de7fa7c04d9b1a1d991905603f8ff8a
foo-io <m.fedorov@edu.sibstrin.ru>
2024-06-09 11:46:33 +0900

    Исправлен баннер. Добавлена страница "Вступить в программу"

--- src/js/main.js
+++ src/js/main.js
@@ -321,3 +321,26 @@ function headerClick() {
 	this.classList.toggle('active');
 	this.nextElementSibling.classList.toggle('spoiler-body');
 }
+
+// Placeholder for dropdowns
+const $placeholder = $("select[placeholder]");
+if ($placeholder.length) {
+  $placeholder.each(function() {
+    const $this = $(this);
+
+    // Initial
+    $this.addClass("placeholder-shown");
+    const placeholder = $this.attr("placeholder");
+    $this.prepend(`<option value="" selected disabled>${placeholder}</option>`);
+
+    // Change
+    $this.on("change", (event) => {
+      const $this = $(event.currentTarget);
+      if ($this.val()) {
+        $this.removeClass("placeholder-shown").addClass("placeholder-hidden");
+      } else {
+        $this.addClass("placeholder-shown").removeClass("placeholder-hidden");
+      }
+    });
+  });
+}
